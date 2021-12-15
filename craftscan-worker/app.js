708ab13300cb0876dbd51b5a51ffc5344dd13b1d
RoyTakanen <roy@imap.fi>
2021-12-15 23:21:49 +0200

    simple worker

--- craftscan-worker/app.js
+++ craftscan-worker/app.js
@@ -1,5 +1,18 @@
 const portscanner = require('portscanner')
+const Netmask = require('netmask').Netmask
+const colors = require('colors/safe')
 
-portscanner.checkPortStatus(25565, 'mc.karanteeni.net', (error, status) => {
-  console.log(status)
-})
+const block = new Netmask('109.204.224.0/24');
+console.log(colors.yellow(`Received ${block.size} ips to scan.`))
+
+const scan = (ip, long, index) => {
+    portscanner.checkPortStatus(25565, ip, (error, status) => {
+        if (status == 'open') {
+            console.log(`Found server at ${ip}`)
+        }
+    })
+}
+
+block.forEach((ip, long, index) => {
+    scan(ip, long, index)
+});

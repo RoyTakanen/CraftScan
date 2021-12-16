08dd6ca9027af7a0b127375d305bc0c951403cd0
RoyTakanen <roy@imap.fi>
2021-12-16 11:14:52 +0200

    improve server

--- craftscan-master/app.js
+++ craftscan-master/app.js
@@ -1,21 +1,11 @@
-const portscanner = require('portscanner')
 const Netmask = require('netmask').Netmask
 const colors = require('colors/safe')
-const mcpinger = require("mcpinger")
 // const { format, toHTML } = require('minecraft-motd-util');
 
 const block = new Netmask('109.204.224.0/24');
 console.log(colors.yellow(`Received ${block.size} ips to scan.`))
 
-// Switch to class and use add to save to mongo
 
-const scan = (ip, long, index) => {
-    portscanner.checkPortStatus(25565, ip, (error, status) => {
-        if (status == 'open') {
-            console.log(`Confirmed server at ${ip}`)
-        }
-    })
-}
 
 block.forEach((ip, long, index) => {
     scan(ip, long, index)
--- craftscan-master/src/server.js
+++ craftscan-master/src/server.js
@@ -1,18 +1,31 @@
 const geoip = require('geoip-lite');
+const portscanner = require('portscanner')
+const mcpinger = require("mcpinger")
 
 class MinecraftServer {
     constructor(serverIp, workerIp) {
         this.serverIp = serverIp
         this.workerIp = workerIp
         this.timestamp = + new Date()
+
+        this.checkIfUp()
     }
 
-    checkGeoIp(ip) {
-        this.geo = geoip.lookup(ip)
+    checkIfUp() {
+        portscanner.checkPortStatus(25565, this.serverIp, (error, status) => {
+            if (status == 'open') {
+                this.portOpen = true
+            }
+        })
     }
 
-    checkMotd(ip) {
-        mcpinger.java({ host: ip }).then((mcping) => {
+    checkGeoIp() {
+        this.serverGeo = geoip.lookup(this.serverIp)
+        this.workerGeo = geoip.lookup(this.workerIp)
+    }
+
+    checkMcping() {
+        mcpinger.java({ host: this.serverIp }).then((mcping) => {
             this.mcping = mcping
         })
     }
